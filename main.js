d54a1ea368701cd8f2809cbded06e8efaa85ec98
flupe <lucas@escot.me>
2024-09-07 22:52:56 +0200

    cleanup + resizing

--- main.js
+++ main.js
@@ -1,120 +1,165 @@
 // TODOS:
 
-// - have several canvases for gizmos + rendered paths, etc
 // - add resize event
 // - add menu
 // - better brush engine
 // - better line smoothing, uniform bezier sampling etc
 
-const on  = (t, es, fn) => {
+// -----------------------------------------------------------------------------
+// utils
+// -----
+
+const on = (t, es, fn) => {
   if (Array.isArray(es)) {
     es.forEach(e => t.addEventListener(e, fn, false))
   }
   else t.addEventListener(es, fn, false)
 }
 
+const {ceil, sin, cos, atan2, PI, sqrt, pow, min, max} = Math
+
 const lerp = (t, a, b)       => a + t * (b - a)
 const bez3 = (t, a, b, c)    => lerp(t, lerp(t, a, b), lerp(t, b, c))
 const bez4 = (t, a, b, c, d) => lerp(t, bez3(t, a, b, c), bez3(t, b, c, d))
 
-let scale  = devicePixelRatio
-let width  = Math.ceil(document.body.clientWidth  * scale)
-let height = Math.ceil(document.body.clientHeight * scale)
+// -----------------------------------------------------------------------------
+// setup
+// -----
 
-const layers = []
+const SCALE = devicePixelRatio
 
-function newLayer(){
-  const dom = document.createElement('canvas')
-  const ctx = dom.getContext('2d')
-
-  dom.width  = width
-  dom.height = height
-
-  let layer = {dom, ctx}
-
-  layers.push(layer)
-
-  return layer
+function layer() {
+  let dom = document.createElement("canvas")
+  let ctx = dom.getContext("2d")
+  return {dom, ctx}
 }
 
-const screen = newLayer()
-document.body.appendChild(screen.dom)
+// viewport & canvas setup
+const viewport = layer()
+const canvas   = layer()
 
-const drawing = newLayer()
-drawing.ctx.globalCompositeOperation = "multiply"
+// viewport dimensions
+let width  = canvas.width  = ceil(document.body.clientWidth  * SCALE)
+let height = canvas.height = ceil(document.body.clientHeight * SCALE)
 
-const guizmos = newLayer()
+// initial g sizing, to make updatable?
+const GIZMO_SIZE = Math.min(2 * height / 5, 2 * width / 5)
 
-let bsize = 7 
+canvas.ctx.globalCompositeOperation = "multiply"
+
+viewport.dom.width  = canvas.dom.width  = width
+viewport.dom.height = canvas.dom.height = height
+
+document.body.appendChild(viewport.dom)
+
+const BRUSH_SIZE = 7  // TODO: make it configurable
 let DRAWING = false
 let HOVER   = false
 let GUIDED  = false
 let ANGLE   = 0
 
-// last 2 known positions (w/ 0 being the most recent)
-let lasts = [{x:0, y:0, s: 0}, {x:0, y:0, s: 0}]
-let mouse = lasts[0]
+let mouse = {
+  x: 0,  // x, y : absolute top-left window coords
+  y: 0,
+  dx: 0, // dx, dy : viewport-local scale-adjusted coords
+  dy: 0,
+}
 
-on(screen.dom, "pointerover", e => { HOVER = true })
-on(screen.dom, "pointerdown", async e => {
+// -----------------------------------------------------------------------------
+// event handlers
+// --------------
+
+function mouseCoords(e) {
+  mouse.x = e.clientX * SCALE
+  mouse.y = e.clientY * SCALE
+
+  mouse.dx = mouse.x - width / 2
+  mouse.dy = height / 2 - mouse.y
+}
+
+on(viewport.dom, "pointerover", e => { HOVER = true })
+on(viewport.dom, "pointerdown", async e => {
   DRAWING = true
-
   GUIDED = e.altKey
 
-  let size = bsize * scale * e.pressure
-  let x = e.clientX * scale
-  let y = e.clientY * scale
+  mouseCoords(e)
+  brushStart(e)
 
-  lasts[0].x = lasts[1].x = x
-  lasts[0].y = lasts[1].y = y
-  lasts[0].s = lasts[1].s = size
-
-  // for now, let's only care about the front vanishing point
-  let dx = x - width / 2
-  let dy = height / 2 - y
-  ANGLE = Math.atan2(dy, dx)
+  // getting gizmo angle
+  if (GUIDED) ANGLE = atan2(mouse.dy, mouse.dx)
 })
 
-on(screen.dom, "pointermove", async e => {
+on(viewport.dom, "pointermove", async e => {
   HOVER = true
+  mouseCoords(e)
 
   if (DRAWING) moveBrush(e)
-  else {
-    mouse.x = e.clientX * scale
-    mouse.y = e.clientY * scale
-  }
 
-  drawGuizmos()
   draw()
 })
 
-on(window, ["pointerup", "blur"], async e => { DRAWING = false })
-on(window, "blur", async e => { HOVER = false })
+on(window, ["pointerup", "blur"], e => { DRAWING = false })
+on(window, "blur", e => { HOVER = false })
+
+on(window, "resize", e => {
+  width  = viewport.dom.width  = ceil(document.body.clientWidth  * SCALE)
+  height = viewport.dom.height = ceil(document.body.clientHeight * SCALE)
+  draw()
+})
+
+// -----------------------------------------------------------------------------
+// brush "engine"
+// --------------
+
+// basic brush texture
+let brush = layer()
+
+brush.dom.width  = brush.dom.height = BRUSH_SIZE
+
+let grad = brush.ctx.createRadialGradient(
+  BRUSH_SIZE / 2, BRUSH_SIZE / 2, BRUSH_SIZE / 4,
+  BRUSH_SIZE / 2, BRUSH_SIZE / 2, BRUSH_SIZE / 2
+)
+
+grad.addColorStop(0, "#000")
+grad.addColorStop(1, "transparent")
+
+brush.ctx.fillStyle = grad
+brush.ctx.fillRect(0, 0, BRUSH_SIZE, BRUSH_SIZE)
+
+// last 2 known positions (w/ 0 being the most recent)
+let lasts = [{x:0, y:0, s: 0}, {x:0, y:0, s: 0}]
+
+function brushStart(e) {
+  // starting new brush stroke
+  let size = BRUSH_SIZE * SCALE * sqrt(e.pressure)
+  lasts[0].x = lasts[1].x = mouse.dx
+  lasts[0].y = lasts[1].y = mouse.dy
+  lasts[0].s = lasts[1].s = size
+}
 
 function moveBrush(e) {
-  let s = bsize * scale * Math.sqrt(e.pressure)
+  const ctx = canvas.ctx
+
+  ctx.save()
+  ctx.translate(canvas.width / 2, canvas.height / 2)
+  ctx.scale(1, -1)
+
+  let s = BRUSH_SIZE * SCALE * sqrt(e.pressure)
 
   let x, y
 
+  // TODO: move this to gizmo logic
   if (GUIDED) {
-    // sticking mouse to line going to the vp
-    let mx = e.clientX * scale
-    let my = e.clientY * scale
-
-    let dx = mx - width / 2
-    let dy = height / 2 - my
-
-    let ca = Math.cos(ANGLE)
-    let sa = Math.sin(ANGLE)
-
-    let p = ca * dx + sa * dy
-
-    x = width  / 2  + ca * p
-    y = height / 2  - sa * p
+    let ca = cos(ANGLE)
+    let sa = sin(ANGLE)
+    let p = ca * mouse.dx + sa * mouse.dy
+    x = ca * p
+    y = sa * p
   }
   else {
-    x = e.clientX * scale
-    y = e.clientY * scale
+    x = mouse.dx
+    y = mouse.dy
   }
 
   // control point as the continuation of the two previous points
@@ -122,9 +167,7 @@ function moveBrush(e) {
   let ctrly = lasts[0].y + .5 * (lasts[0].y - lasts[1].y)
 
   // of course now derivative of the curve is discontinuous at each new point
-  // so is it really smoothing?
 
-  const ctx = drawing.ctx
   ctx.globalAlpha = e.pressure
 
   let steps = Math.max(10, Math.sqrt(Math.pow(x - lasts[0].x, 2) +  Math.pow(y - lasts[0].y, 2)) / 2)
@@ -136,7 +179,7 @@ function moveBrush(e) {
     let yy = bez3(r, lasts[0].y, ctrly, y)
     let ss = lerp(r, lasts[0].s, s)
 
-    ctx.drawImage(bcvs, 0, 0, bsize, bsize, xx - ss / 2, yy - ss / 2, ss, ss)
+    ctx.drawImage(brush.dom, 0, 0, BRUSH_SIZE, BRUSH_SIZE, xx - ss / 2, yy - ss / 2, ss, ss)
   }
 
   // swap last positions, update
@@ -146,41 +189,39 @@ function moveBrush(e) {
   l.y = y
   l.s = s
   lasts[0] = l
+  ctx.restore()
 }
 
-function drawGuizmos() {
-  const ctx = guizmos.ctx
-  ctx.clearRect(0, 0, width, height)
-  ctx.save()
+// -----------------------------------------------------------------------------
+// gizmos
+// ------
 
-  ctx.translate(width / 2, height / 2)
-  ctx.scale(1, -1)
+function drawGizmos() {
+  const ctx = viewport.ctx
 
   ctx.strokeStyle = "#f0f"
-  ctx.lineWidth = 2 * scale
+  ctx.lineWidth = 2 * SCALE
 
-  let dist = Math.min(2 * height / 5, 2 * width / 5)
 
   // vanishing points
-
   ctx.beginPath()
   ctx.arc(    0,     0, 5, 0, 2 * Math.PI)
   ctx.stroke()
   ctx.beginPath()
-  ctx.arc(    0,  dist, 5, 0, 2 * Math.PI)
+  ctx.arc(    0,  GIZMO_SIZE, 5, 0, 2 * Math.PI)
   ctx.stroke()
   ctx.beginPath()
-  ctx.arc(    0, -dist, 5, 0, 2 * Math.PI)
+  ctx.arc(    0, -GIZMO_SIZE, 5, 0, 2 * Math.PI)
   ctx.stroke()
   ctx.beginPath()
-  ctx.arc( dist,     0, 5, 0, 2 * Math.PI)
+  ctx.arc( GIZMO_SIZE,     0, 5, 0, 2 * Math.PI)
   ctx.stroke()
   ctx.beginPath()
-  ctx.arc(-dist,     0, 5, 0, 2 * Math.PI)
+  ctx.arc(-GIZMO_SIZE,     0, 5, 0, 2 * Math.PI)
   ctx.stroke()
-  ctx.lineWidth = scale
+  ctx.lineWidth = SCALE
   ctx.beginPath()
-  ctx.arc(0, 0, dist, 0, 2 * Math.PI)
+  ctx.arc(0, 0, GIZMO_SIZE, 0, 2 * Math.PI)
   ctx.stroke()
 
   let dx = mouse.x - width / 2
@@ -191,7 +232,7 @@ function drawGuizmos() {
 
   if (HOVER) {
     ctx.beginPath()
-    let angle = DRAWING ? ANGLE : Math.atan2(dy, dx)
+    let angle = DRAWING && GUIDED ? ANGLE : Math.atan2(dy, dx)
     let ca = Math.cos(angle)
     let sa = Math.sin(angle)
 
@@ -202,31 +243,24 @@ function drawGuizmos() {
     ctx.stroke()
   }
 
+}
+
+function draw() {
+  let ctx = viewport.ctx
+  ctx.clearRect(0, 0, width, height)
+
+  ctx.save()
+  // TODO: viewport offset
+  ctx.translate(width / 2, height / 2)
+  // TODO: viewport zoom
+
+  ctx.fillStyle = '#aaa'
+  ctx.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height)
+  ctx.drawImage(canvas.dom, -canvas.width / 2, -canvas.height / 2)
+  ctx.scale(1, -1)
+  drawGizmos()
+
   ctx.restore()
 }
 
-// update the entire display
-function draw() {
-  screen.ctx.clearRect(0, 0, width, height)
-  screen.ctx.drawImage(drawing.dom, 0, 0)
-  screen.ctx.drawImage(guizmos.dom, 0, 0)
-}
-
-// basic brush shape canvas
-let bcvs = document.createElement("canvas")
-let bctx = bcvs.getContext("2d")
-
-bcvs.width  = bsize
-bcvs.height = bsize
-
-let grad = bctx.createRadialGradient(bsize / 2, bsize / 2, bsize / 4,
-                                     bsize / 2, bsize / 2, bsize / 2)
-
-grad.addColorStop(0, "#000")
-grad.addColorStop(1, "transparent")
-
-bctx.fillStyle = grad
-bctx.fillRect(0, 0, bsize, bsize)
-
-drawGuizmos()
 draw()
